1dade66b7a7a1648afcecb7d6f9a0f9286d4a22d
Korneliu <olbiton@gmail.com>
2022-03-12 20:13:59 -0800

    added Navbar component

---
+++ components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class Navbar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='navbar-container'>
+        <h1>Test App</h1>
+      </div>
+    )
+  }
+}
