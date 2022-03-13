440caca04e821cdbbcd6e46d21ab8347d7ad3f7b
Korneliu <olbiton@gmail.com>
2022-03-12 20:17:46 -0800

    added detal-info page

---
+++ pages/detail-info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class DetailInfo extends React.Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h2>Detail Info</h2>
+      </div>
+    )
+  }
+}
