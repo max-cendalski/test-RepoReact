7e037b29a6729f1e7571280b51a28d845d06a1df
Korneliu <olbiton@gmail.com>
2022-03-20 21:02:33 -0700

    added links to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Navbar(props) {
     return (
       <div>
-        <h2>Navbar</h2>
+        <a href="www.google.com">Films</a>
+        <a href="www.google.com">Places</a>
       </div>
     )
 }
