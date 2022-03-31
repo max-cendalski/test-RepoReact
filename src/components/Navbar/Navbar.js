fdad36eb1e2a2b3f3c9c4b249fe1e6223feb6b7f
Korneliu <olbiton@gmail.com>
2022-03-30 19:42:14 -0700

    added .env file

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar(props) {
     return (
-      <div>
+      <div className='navbar'>
         <a href="www.google.com">Films</a>
         <a href="www.google.com">Places</a>
       </div>
