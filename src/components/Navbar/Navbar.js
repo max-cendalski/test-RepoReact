3aab6d51ab2579e22de8e46252b24a32b4a13491
Korneliu <olbiton@gmail.com>
2022-04-16 21:42:11 -0700

    added display:grid to #root

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
-      <nav className='navbar'>
+      <nav id='navbar'>
         <Link to="/films">Films</Link>
         <Link to="/places">Places</Link>
         <Link to="/">Home</Link>
