283880b0de139c42f12c94fdd6a961d6d9343595
Korneliu <olbiton@gmail.com>
2022-05-04 20:31:45 -0700

    changed images location

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel';
 
 export default class CarouselComponent extends React.Component {
   render() {
         return (
           <>
             <Navbar />
+                <Carousel
+                autoPlay={true}
+                infiniteLoop={true}
+                >
+                <div>
+                    <img src="../../images/Greece1.jpg" alt="greece1" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="../../images/Greece2.jpg" alt="greece2" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                     <img src="../../images/Greece3.jpg" alt="greece3" />
 
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
           </>
         )
   }
--- src/components/Films/Films.jsx
+++ src/components/Films/Films.jsx
@@ -1,11 +1,17 @@
 import "./films.css"
 import Navbar from "../Navbar/Navbar"
-
 export default function Films() {
   return (
+    <>
+
     <div>
       <Navbar />
       <h1>Films</h1>
+
+     <img src="../../images/Greece1.jpg" alt="greece1"></img>
+
+
     </div>
+      </>
   )
 }
