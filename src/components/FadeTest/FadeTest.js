4f98f0742c6d54383cc2d6cfe3f426937a54bfce
Korneliu <olbiton@gmail.com>
2022-05-07 22:03:15 -0700

    changed to class component

--- src/components/FadeTest/FadeTest.js
+++ src/components/FadeTest/FadeTest.js
@@ -1,8 +1,28 @@
+import './fade.css'
 import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import { useState, useEffect } from 'react'
 
 
-export default function FadeTest () {
+export default class FadeTest extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      photo: 'invisible'
+    }
+  }
+
+
+render() {
   return (
-    <h1>Fade</h1>
+    <article>
+    <Navbar />
+      <h1>Fade</h1>
+      <section className={this.state.photo}></section>
+    </article>
+
+
   )
+}
+
 }
