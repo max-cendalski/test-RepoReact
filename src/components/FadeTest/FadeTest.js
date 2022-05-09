1395288af46e1d2411579fe5bb4394d2405de6f5
Korneliu <olbiton@gmail.com>
2022-05-09 10:34:46 -0700

    removed props

--- src/components/FadeTest/FadeTest.js
+++ src/components/FadeTest/FadeTest.js
@@ -8,15 +8,24 @@ export default class FadeTest extends React.Component {
   constructor(props) {
     super(props)
     this.state ={
-      photo: 'photo-section'
+      photo: 'photo-section',
+      fadeState: false
     }
   }
 
  componentDidMount() {
  setTimeout(()=> {
+   if (this.state.fadeState === false) {
    this.setState({
-     photo: 'photo-section2'
+     photo: 'photo-section2',
+     fadeState: true
    })
+   } else {
+      this.setState({
+     photo: 'photo-section'
+   })
+   }
+
  },100)
 }
 
@@ -27,7 +36,6 @@ render() {
       <h1>Fade</h1>
       <section className='image-container'>
       <img src='../../images/Greece2.jpg' className={this.state.photo} alt="greec1"/>
-
       </section>
     </article>
   )
