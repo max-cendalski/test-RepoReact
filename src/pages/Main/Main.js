979e7eb85a4e6cd91f830fa397bf09d5950c6e0f
Korneliu <olbiton@gmail.com>
2022-05-30 20:36:53 -0700

    added List to Homepage

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,21 @@
 import './main.css'
 import Counter from '../../components/features/counter/Counter'
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
+import List from '../../components/List'
 
 
 
 export default function Main(props) {
+const notes = useSelector((state)=> state.notes.notes)
+console.log('notes',notes)
+
 
   return (
     <article id="main-container">
       <h1>Homepage</h1>
       <Counter />
+      <List notes={notes} />
     </article>
   )
 }
