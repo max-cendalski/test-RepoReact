a3220725f0b2a84c2e9a9a8e48374edd3e4ac1fd
Korneliu <olbiton@gmail.com>
2022-06-01 16:11:18 -0700

    added users select form control

--- src/components/features/posts/AddPostForm.js
+++ src/components/features/posts/AddPostForm.js
@@ -1,27 +1,38 @@
 import './posts.css'
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { nanoid } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 
 import { postAdded} from './postsSlice'
+import { selectAllUsers } from '../users/usersSlice';
 
 const AddPostForm = () => {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [userId, setUserId] = useState('')
+
   const dispatch = useDispatch()
+
+  const users = useSelector(selectAllUsers)
 
   const onTitleChanged = e => setTitle(e.target.value)
   const onContentChanged = e => setContent(e.target.value)
+  const onAuthorChanged = e => setUserId(e.target.value)
 
   const onSavePostClicked =()=> {
     if (title && content) {
       dispatch(
-        postAdded(title,content)
+        postAdded(title,content, userId)
       )
       setTitle('')
       setContent('')
     }
   }
+
+  const usersOptions = users.map(user=> {
+    <option key={user.id} value={user.id}>
+      {user.name}}
+    </option>
+  })
 
   return (
     <section>
@@ -36,6 +47,13 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+      </p>
+      <p>
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
       </p>
       <p>
          <label htmlFor="postContent">Content:</label>
