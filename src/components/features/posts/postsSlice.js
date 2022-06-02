5ea1840f2bfca67892eae88a1e74782bd48f84f4
Korneliu <olbiton@gmail.com>
2022-06-02 11:01:08 -0700

    added date to postsSlice component

--- src/components/features/posts/postsSlice.js
+++ src/components/features/posts/postsSlice.js
@@ -30,6 +30,7 @@ const postsSlice = createSlice({
             id:nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId
           }
         }
