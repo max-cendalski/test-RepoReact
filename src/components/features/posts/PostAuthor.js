4cc1eafdab7e912a44ac0223b0c0354638c2045f
Korneliu <olbiton@gmail.com>
2022-06-02 10:45:51 -0700

    add PostAuthor component

---
+++ src/components/features/posts/PostAuthor.js
@@ -0,0 +1,10 @@
+import { useSelector } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
+
+const PostAuthor = () => {
+  return (
+    <article>
+      PostAuthor
+    </article>
+  )
+}
