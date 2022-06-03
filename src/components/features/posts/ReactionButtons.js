211ef3bcc5bdd7a560975ece237d76bd46b648d0
Korneliu <olbiton@gmail.com>
2022-06-02 19:32:57 -0700

    added ReactionButtons component

---
+++ src/components/features/posts/ReactionButtons.js
@@ -0,0 +1,18 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
+
+const ReactionEmoji = {
+  thumbsUp: '👍',
+  wow: '😊',
+  heart: '❤️',
+  rocket: '🚀',
+  coffee: '🍷'
+}
+
+const ReactionButtons = () => {
+  return (
+
+  )
+}
+
+export default ReactionButtons
