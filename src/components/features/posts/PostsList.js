b6883f2c687f3d56485d3e7846f6668aca4e3654
Korneliu <olbiton@gmail.com>
2022-06-02 19:49:46 -0700

    added ReactionButtons to PostList component

--- src/components/features/posts/PostsList.js
+++ src/components/features/posts/PostsList.js
@@ -5,6 +5,7 @@ import AddPostForm from './AddPostForm';
 import Users from '../users/Users';
 import PostAuthor from './PostAuthor';
 import TimeAgo from './TimeAgo';
+import ReactionButtons from './ReactionButtons';
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts)
@@ -19,6 +20,7 @@ const PostsList = () => {
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   )
   )
