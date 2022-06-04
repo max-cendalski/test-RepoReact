5370ff6e515b503875293b0b7db6dda839b81333
Korneliu <olbiton@gmail.com>
2022-06-03 20:03:01 -0700

    added fetchUsers function

--- src/components/features/users/usersSlice.js
+++ src/components/features/users/usersSlice.js
@@ -1,13 +1,19 @@
 import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
 import axios from "axios";
 
-const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users';
 
-const initialState=[
-  {id: '0',name: 'Max Cendalski'},
-  {id: '1', name:'Dave Gray'},
-  {id: '2',name: 'Jeremi Szpak'}
-]
+const initialState=[]
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers',async ()=> {
+  try{
+    const response = await axios.get(USERS_URL)
+    return response.data
+  } catch(err) {
+    return err.message
+  }
+})
+
 const usersSlice = createSlice({
   name: 'users',
   initialState,
