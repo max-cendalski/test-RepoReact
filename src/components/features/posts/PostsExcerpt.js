2551e04baf728175023221d718560dab622ca876
Korneliu <olbiton@gmail.com>
2022-06-03 17:42:52 -0700

    created PostsExcerpt component

---
+++ src/components/features/posts/PostsExcerpt.js
@@ -0,0 +1,14 @@
+import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
+import ReactionButtons from './ReactionButtons';
+
+
+const PostsExcerpt = ({post}) => {
+  return (
+    <article>
+      PostsExcerpt
+    </article>
+  )
+}
+
+export default PostsExcerpt
--- src/components/features/posts/PostsList.js
+++ src/components/features/posts/PostsList.js
@@ -1,11 +1,10 @@
 import './posts.css'
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from 'react';
+
 import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from './postsSlice';
 import AddPostForm from './AddPostForm';
-import PostAuthor from './PostAuthor';
-import TimeAgo from './TimeAgo';
-import ReactionButtons from './ReactionButtons';
+
 
 
 
@@ -16,7 +15,7 @@ const PostsList = () => {
   const postsStatus = useSelector(getPostsStatus)
   const error = useSelector(getPostsError);
 
-  useEffect(() =>{
+  useEffect(() => {
     if (postsStatus === 'idle') {
       dispatch(fetchPosts())
     }
