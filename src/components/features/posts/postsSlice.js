b36f82b225c5e87448bde1cf450567d08c354871
Korneliu <olbiton@gmail.com>
2022-06-08 20:32:18 -0700

    fixed updatePost

--- src/components/features/posts/postsSlice.js
+++ src/components/features/posts/postsSlice.js
@@ -33,13 +33,14 @@ export const addNewPost = createAsyncThunk('posts/addNewPost', async (initialPos
   }
 })
 
-export const updatePost = createAsyncThunk('posts/updatePost', async(initialPost) ={
-const {id} = initialPost);
-try{
-  const response = await.axios.put(`${POSTS_URL}/${id}`,initialPost)
-  return response.data
-} catch (err) {
-  return err.message
+export const updatePost = createAsyncThunk('posts/updatePost', async (initialPost) => {
+  const {id} = initialPost;
+  try {
+    const response = await axios.put(`${POSTS_URL}/${id}`,initialPost)
+    return response.data
+  } catch (err) {
+    return err.message
+  }
 })
 
 const postsSlice = createSlice({
