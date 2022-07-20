6e015c63ab8fc18141f67a6761fa0cbfa497e5c3
Korneliu <olbiton@gmail.com>
2022-07-19 20:19:08 -0700

    added getTasks to useEffect

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,24 +1,56 @@
 import { useState, useEffect } from "react";
-import {collection, getDocs } from 'firebase/firestore';
+import {collection, getDocs,getDoc, addDoc, doc } from 'firebase/firestore';
 import {db} from '../../components/firebase/Firebase';
 
 
 
 const Tasks = () => {
  const [tasks, setTasks] = useState([])
- const tasksDb = collection(db, 'tasks')
+ const [users, setUsers] = useState([])
+ const [user, setUser] = useState([])
+ const tasksCollection = collection(db, 'tasks')
+ const usersDb = collection(db, 'users')
 
     useEffect(()  => {
     const getTasks = async() => {
-      const tasksList = await getDocs(tasksDb)
-      console.log('tasksList',tasksList)
+      const querySnapshot = await getDocs(tasksCollection);
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
     }
     getTasks()
   },[])
+
+  const handleRetrieveUsers = async() => {
+    const userRef = doc(db, "users", "Uu8m5sAOkjOCmkUAVfGs")
+    const userSnap = await getDoc(userRef)
+    if (userSnap.exists()) {
+      setUser(userSnap.data())
+      console.log("Document data:", userSnap.data());
+      } else {
+      console.log("No such document!");
+    }
+  }
+  const handleAddTask = async () => {
+    try {
+      const addTask = await addDoc(tasksCollection, {
+        date: 'July 20th',
+        note: 'by end of the week',
+        title: 'Clean the floor'
+      })
+      console.log("Task added with ID: ", addTask.id)
+    } catch(e) {
+      console.error("ERROR: ",e)
+    }
+  }
 
   return (
     <article>
       <h1>Tasks</h1>
+      <button onClick={handleRetrieveUsers}>Click to render user</button>
+      <button onClick={handleAddTask}>Click to Add Task</button>
+      <h2>{user.name}</h2>
+      <h2>{user.lastName}</h2>
     </article>
   )
 }
@@ -28,24 +60,21 @@ const Tasks = () => {
 export default Tasks;
 
 
-/* const Tasks = () => {
-  const [tasks, setTask] = useState([])
-  const tasksDb = collection(firestore, 'tasks')
-
-  useEffect(()  => {
+// WORKING CODE
+/*  const addUser = async () => {
+      try {
+        const userRef = await addDoc(collection(db, "users"), {
+          name: 'Max',
+          lastName: 'Cendalski'
+        })
+        console.log("User added with ID: ", userRef.id)
+      } catch (e) {
+        console.error("Error adding user :", e)
+      }
+    }
     const getTasks = async() => {
       const tasksList = await getDocs(tasksDb)
-      console.log('tasksList')
+      console.log('tasksList',tasksList)
     }
-    getTasks()
-  },[])
-
-  return (
-    <article>
-      <h1>Tasks List</h1>
-    </article>
-  )
-}
-
-export default Tasks;
- */
+    addUser()
+    getTasks() */
