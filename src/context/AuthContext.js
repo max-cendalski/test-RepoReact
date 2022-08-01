e79f5f3605c04e098f34dec1af47d261ebef390d
Korneliu <olbiton@gmail.com>
2022-08-01 16:57:55 -0700

    added SignIn page

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,5 +1,6 @@
 import { useContext, createContext, useEffect, useState } from 'react';
-import {GoogleAuthProvider} from 'firebase/auth';
+import {GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut, onAuthStateChanged} from 'firebase/auth';
+import auth from '../components/firebase/Firebase';
 
 
 
@@ -8,6 +9,15 @@ const AuthContext = createContext()
 
 export const AuthContextProvider = ({children}) => {
   const credentials = {name:'Maximilian',lastName:'Cendalski'}
+
+  const googleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result)
+      const token = credential.accessToken;
+      const user = result.user;
+    })
+  }
 
     return (
       <AuthContext.Provider value={credentials}>
---
+++ src/pages/SignIn/SignInPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {UserAuth} from '../../context/AuthContext';
+
+cons SignIn = () => {
+  return (
+    <article>
+      <h1>SignIN</h1>
+      <button>Sign With Google</button>
+    </article>
+  )
+}
+
+export default SignIn
