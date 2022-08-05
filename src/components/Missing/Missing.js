864a907604744034f134c90232dd43a8cd132d88
Korneliu <olbiton@gmail.com>
2022-08-04 19:59:18 -0700

    added link to homepage

--- src/components/Missing/Missing.js
+++ src/components/Missing/Missing.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const  Missing =() => {
   return (
     <article>
-      <h1>Something went wrong</h1>
+      <h1>Something went terrible wrong!!!</h1>
+      <Link to ="/">Click to visit Homepage</Link>
     </article>
   )
 }
